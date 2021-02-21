0c669278b188a5beeb07e4a07e6e7445143c097a
Dan McKeon <dpmckeon@gmail.com>
2021-02-21 10:37:41 -0800

    successfully retrieve greenhouse heater and den heater

--- lib/kasa.ts
+++ lib/kasa.ts
@@ -1,11 +1,18 @@
 import { login } from "tplink-cloud-api";
 
-export const init = async () => {
+export const createTpLinkInstance = async () => {
   return await login(process.env.TP_LINK_EMAIL, process.env.TP_LINK_PASSWORD);
 }
 
-export const getDevices = async() => {
-  const tpLink =  await init();
+export const getDenHeater = async (tpLink) => {
+  return await tpLink.getHS100('Den Heater');
+}
+
+export const getGreenhouseHeater = async (tpLink) => {
+  return await tpLink.getHS100('Greenhouse heater');
+}
+
+export const getDevices = async(tpLink) => {
   const deviceList = await tpLink.getDeviceList();
   return deviceList;
 }
--- tasks/kasa-playground.ts
+++ tasks/kasa-playground.ts
@@ -1,12 +1,19 @@
 #!/usr/bin/env ts-node -T
 
-import {getDevices} from '../lib/kasa'
+import {getDevices, createTpLinkInstance, getDenHeater, getGreenhouseHeater} from '../lib/kasa'
 
 const main = async () => {
   console.log('kasa-playground');
+  const tpLink = await createTpLinkInstance();
 
-  const devices = await getDevices();
+  const devices = await getDevices(tpLink);
   console.log('devices', devices);
+
+  const denHeater = await getDenHeater(tpLink);
+  console.log('denHeater', denHeater);
+
+  const greenhouseHeater = await getGreenhouseHeater(tpLink);
+  console.log('greenhouseHeater', greenhouseHeater);
 };
 
 main()
